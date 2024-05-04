a12c3d07d9ff1a8dc92e16604df8876e402e9ba1
Benjamin Kaitany <benjaminkipkem@gmail.com>
2024-05-04 17:19:22 +0300

    update on main.py & inventory.db

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import streamlit as st
 import sqlite3
 
+# Define class 'Inventory'
 class Inventory:
+    # Define Class constructor 'self'
     def __init__(self):
+        #create a database connection object 'inventory.db'
         self.conn = sqlite3.connect('inventory.db')
         self.create_table()
-
+    
+    #Define class methods
     def create_table(self):
+        #create 'cursor' object
         cursor = self.conn.cursor()
+        #call of cursor objects' 'execute' method
         cursor.execute('''CREATE TABLE IF NOT EXISTS products (
                             name TEXT PRIMARY KEY,
                             quantity INTEGER
@@ -17,7 +23,7 @@ class Inventory:
     def add_product_to_db(self, name, quantity):
         #capitalize name
         name=name.capitalize()
-        
+
         cursor = self.conn.cursor()
         cursor.execute('''SELECT quantity FROM products WHERE name=?''', (name,))
         row = cursor.fetchone()
@@ -92,22 +98,35 @@ st.set_page_config(
 )    
 
 def main():
-    st.title("Streamlit Inventory Tracker: Automated SQLite Database Support")
+    st.title("Streamlit Inventory Tracker:dart:") 
+    st.header(":rainbow[Automated SQLite Database Support]:sparkles:")
     st.image("image/taifa2image.jpg")
-
+#'_Streamlit_ is :blue[cool] :sunglasses:'
     inventory = Inventory()
 
-    option = st.selectbox("**Select Action**", ["Add Product", "Sell Product", "View Stock", "Adjust Quantity", "Delete Product"])
+    option = st.selectbox("**Select Action** :small_red_triangle_down:", ["Add Product", "Sell Product", "View Stock", "Adjust Quantity", "Delete Product"])
 
+    
     if option == "Add Product":
         st.header("Add Product")
-        product_name = st.text_input("**Product Name**")
-        product_quantity = st.number_input("**Quantity**", min_value=1, step=1)  # Minimum value set to 1
+
+    # Define the list of available products
+        predefined_products = ["Taifa 1/2kg","Taifa 1kg", "Taifa 2kg", "Bahari 1kg", "Bahari 2kg",
+                                "Bakers 12.5kg", "Bakers 25kg", "Bakers 50kg", "Chenga 1kg"]
+
+    # Dropdown to select product
+        product_name = st.selectbox("**Choose Product Name** :arrow_down_small:", predefined_products)
+
+        product_quantity = st.number_input("**Quantity to Add**", min_value=1, step=1)  # Minimum value set to 1
+
         if st.button("**Add Product**"):
-            if product_quantity <= 0:  # Check if quantity is zero or negative
-              st.error("Quantity must be a positive integer.")
+            # Check if product name is provided
+            if not product_name:  # If product name is empty
+                st.error("Please select a product.")
+            elif product_quantity <= 0:  # Check if quantity is zero or negative
+                st.error("Quantity must be a positive integer.", icon="🚨")
             else:
-               inventory.add_product_to_db(product_name, product_quantity)
+                inventory.add_product_to_db(product_name, product_quantity)
 
     elif option == "Sell Product":
         st.header("Sell Product")
@@ -115,7 +134,7 @@ def main():
         if not product_names:
             st.write("No products available for sale.")
         else:
-            product_name = st.selectbox("**Select Product to Sell**", product_names)
+            product_name = st.selectbox("**Select Product to Sell** :arrow_down_small:", product_names)
             product_quantity = st.number_input("**Quantity to sell**", min_value=1, step=1)
             if st.button("**Sell Product**"):
                 inventory.sell_product_from_db(product_name, product_quantity)
@@ -125,24 +144,24 @@ def main():
         if not product_names:
             st.write("No products available for deletion.")
         else:
-            product_to_delete = st.selectbox("**Select Product to Delete**", product_names)
+            product_to_delete = st.selectbox("**Select Product to Delete** :arrow_down_small:", product_names)
             if st.button("**Delete Product**"):
-                inventory.delete_product(product_to_delete)
+                inventory.delete_product(product_to_delete) 
     elif option == "Adjust Quantity":
-        st.header("Adjust Quantity")
+        st.header("Adjust Quantity: (Decrease)")
         product_names = inventory.get_product_names()
         if not product_names:
             st.write("No products available for adjustment.")
         else:
-            product_to_adjust = st.selectbox("**Select Product to Adjust**", product_names)
+            product_to_adjust = st.selectbox("**Select Product to Adjust** :arrow_down_small:", product_names)
             new_quantity = st.number_input("**Enter New Quantity**", min_value=0, step=1)  # Minimum value set to 0
         if st.button("Adjust Quantity"):
             if new_quantity < 0:  # Check if quantity is negative
-                st.error("Quantity must be a non-negative integer.")
+                st.error("Quantity must be a non-negative integer.", icon="🚨")
             else:
                 inventory.adjust_quantity(product_to_adjust, new_quantity)
     elif option == "View Stock":
-        st.subheader("**Current Inventory**")
+        st.subheader("**Current Inventory** :open_file_folder:")
         cursor = inventory.conn.cursor()
         cursor.execute('''SELECT * FROM products''')
         rows = cursor.fetchall()
